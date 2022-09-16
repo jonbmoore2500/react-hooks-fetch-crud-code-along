0c583e41a63385bb8b5e080e54f2cc958ff36dc4
Jonathan <jonathanmoorevla@gmail.com>
2022-09-16 12:15:15 -0500

    Handle adding new item

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -6,12 +6,16 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+  
   useEffect(() => {
     fetch("http://localhost:4000/items")
     .then((r) => r.json())
     .then((items) => setItems(items));
   }, [])
 
+  function handleAddItem(newItem) {
+    setItems([...items, newItem])
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -25,7 +29,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
